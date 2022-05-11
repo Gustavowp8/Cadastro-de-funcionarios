83ea84da35a5a6563faa6d3564426b19b64e7261
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2022-05-11 20:25:07 -0300

    Add files via upload

---
+++ Funcionarios/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioListasEmSala_10_05_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char opcao;
+
+            List<Funcionario> func = new List<Funcionario>();
+            do
+            {
+                Console.Write("Nome do Funcionario: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Valor do salario: ");
+                double salario = double.Parse(Console.ReadLine());
+
+                Console.Write("EIdade: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Departamento de trabalho: ");
+                string departamento = Console.ReadLine();
+                departamento.ToLower();
+
+                func.Add(new Funcionario(nome, salario, idade, email, departamento));
+
+                Console.Write("Deseja adiciona mais um funcionario? (s/n)");
+                opcao = char.Parse(Console.ReadLine());
+
+
+            } while (opcao == 's');
+
+            Console.WriteLine("====== Detalhes ======");
+
+            foreach (Funcionario obj in func)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("============");
+
+            Console.WriteLine("Total de funcionarios: " + func.Count);
+
+            int contador = 0;
+            if (func.Exists(x => x.Departamento == "rh"))
+            {
+                foreach (Funcionario obj in func)
+                {
+                    contador++;
+                }
+                Console.WriteLine(contador);
+            }
+
+
+
+
+
+        }
+    }
+}
